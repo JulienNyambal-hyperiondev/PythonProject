34c5260bb25defaa2b6b498463fa8cb57dab5511
Julien Nyambal <julienn@hyperiondev.com>
2024-09-05 21:50:17 +0200

    updated loops file

---
+++ loops.py
@@ -0,0 +1,16 @@
+numbers = [1,2,3,4,5]
+
+total = 0
+counter = 0
+
+while counter < len(numbers):
+    total = total + numbers[counter]
+    print(f"The current number is {counter} and the total is {total}")
+    counter = counter + 1
+print(f"The total is {total}")
+
+
+# contacts = ["Julien", "Riaan", "Armand"]
+
+# for contact in contacts:
+#     print(f"Contact : {contact}")
